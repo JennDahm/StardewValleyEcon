d7d35394c03ed152e2e7109968b69b899b5a5b00
Jennifer Dahm <jdahm@utexas.edu>
2017-12-24 02:10:53 -0600

    Replaced game-provided hover text function with my own that allows icons to be presented in-line. Still needs some work, but looking good.

--- StardewEcon/NewsBulletinMenu.cs
+++ StardewEcon/NewsBulletinMenu.cs
@@ -86,7 +86,7 @@ namespace StardewEcon
             // Draw hover text!
             if (!string.IsNullOrEmpty(this.hoverText))
             {
-                IClickableMenu.drawHoverText(b, hoverText, Game1.smallFont);
+                Utility.DrawHoverTextWithItem(b, "+20%", Game1.smallFont, StardewValley.Object.stone);
             }
         }
 
