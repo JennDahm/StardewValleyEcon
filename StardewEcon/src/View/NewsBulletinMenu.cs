2336dbfe5c297a3aec77a28b5cb26c4337763064
Jennifer Dahm <jdahm@utexas.edu>
2018-01-07 20:32:22 -0600

    Refactored and fiddled with the menu. Added significant documentation to NewsBulletinObject.

--- StardewEcon/src/View/NewsBulletinMenu.cs
+++ StardewEcon/src/View/NewsBulletinMenu.cs
@@ -10,39 +10,91 @@ using StardewEcon.Econ;
 
 namespace StardewEcon.View
 {
+    /**
+     * <summary>A pop-up menu to display the current events.</summary>
+     * <remarks>
+     *  Instances of this class are not meant to be long-lived. It's written
+     *  with the expectation that you create an instance when the user opens
+     *  the menu and that you abandon that instance when the user closes it.
+     *  
+     *  <see cref="NewsBulletinObject"/> is an invisible object for users to
+     *  interact with to tsee this menu. This mod will place one over the sign
+     *  that normally displays Pierre's hours.
+     * </remarks>
+     * 
+     * <seealso cref="NewsBulletinObject"/>
+     */
     public class NewsBulletinMenu : IClickableMenu
     {
-        private int internalWidth;
-        private int internalHeight;
-        private int xOffsetToInternal;
-        private int yOffsetToInternal;
-        private int itemHeight;
-        private int separatorHeight;
-        
+        #region Dimensions
+        /**
+         * <summary>The width of the menu inside the borders, in pixels.</summary>
+         */
+        //private int internalWidth;
+
+        /**
+         * <summary>The width of the menu inside the borders, in pixels.</summary>
+         */
+        //private int internalHeight;
+
+        /**
+         * <summary>The X offset from the upper-left corner of the bounding box to the box inside the menu's borders, in pixels.</summary>
+         */
+        //private int xOffsetToInternal;
+
+        /**
+         * <summary>The Y offset from the upper-left corner of the bounding box to the box inside the menu's borders, in pixels.</summary>
+         */
+        //private int yOffsetToInternal;
+
+        /**
+         * <summary>The height of each event section, in pixels.</summary>
+         */
+        //private int sectionHeight;
+
+        /**
+         * <summary>The height of the separators between event sections, in pixels.</summary>
+         */
+        //private int separatorHeight;
+
+        /**
+         * <summary>The dimensions of this menu.</summary>
+         */
+        private MenuDimensions dims;
+        #endregion
+
+        /**
+         * <summary>A hover box describing an event, if one is moused-over.</summary>
+         */
         private EconEventHoverBox hoverBox;
 
+        /**
+         * <summary>The events displayed by this menu.</summary>
+         */
         private IList<EconEvent> events;
 
+        /**
+         * <summary>Creates a new News Bulletin to show the given events.</summary>
+         * <remarks>
+         *  This constructor will tell the game to play sound - it's expected
+         *  that it is called once per opening of this menu at the time the user
+         *  requests to open it.
+         * </remarks>
+         * 
+         * <param name="events">The events to show.</param>
+         */
         public NewsBulletinMenu(IEnumerable<EconEvent> events)
             : base(x: 0, y: 0, width: 0, height: 0, showUpperRightCloseButton: true)
         {
             this.events = new List<EconEvent>(events);
+            this.hoverBox = null;
+            this.dims = new MenuDimensions(this.events.Count, Game1.pixelZoom);
 
-            // Calculate some numbers
-            int numItems = this.events.Count;
-            this.separatorHeight = 4 * Game1.pixelZoom;
-            this.itemHeight = 50 * Game1.pixelZoom;
-
-            this.internalWidth = 100 * Game1.pixelZoom;
-            this.internalHeight = separatorHeight * (numItems-1) + itemHeight * (numItems);
-            this.xOffsetToInternal = IClickableMenu.borderWidth;
-            this.yOffsetToInternal = IClickableMenu.borderWidth + 7 * Game1.pixelZoom;
-
-            // Calculate width, height, position on screen:
-            this.width = this.internalWidth + 1 * Game1.pixelZoom + IClickableMenu.borderWidth * 2;
-            this.height = this.internalHeight + 5 * Game1.pixelZoom + IClickableMenu.borderWidth * 2;
-            this.xPositionOnScreen = Game1.viewport.Width / 2 - this.width / 2;
-            this.yPositionOnScreen = Game1.viewport.Height / 2 - this.height / 2;
+            var outerBox = this.dims.OuterBox();
+            this.width = outerBox.Width;
+            this.height = outerBox.Height;
+            this.xPositionOnScreen = outerBox.X;
+            this.yPositionOnScreen = outerBox.Y;
 
             // Reinitialize close button:
             this.initializeUpperRightCloseButton();
@@ -51,37 +103,42 @@ namespace StardewEcon.View
             Game1.playSound("bigSelect");
         }
 
+        /**
+         * <summary>Draws the menu.</summary>
+         */
         public override void draw(SpriteBatch b)
         {
-            var content = GetContent().ToList();
+            this.dims.SetZoom(Game1.pixelZoom);
 
             // Draw box and background fade
+            var dialogueBox = this.dims.DialogueBox();
+            var labelPt = this.dims.MenuLabelPoint();
             b.Draw(Game1.fadeToBlackRect, Game1.graphics.GraphicsDevice.Viewport.Bounds, Color.Black * 0.4f);
             Game1.drawDialogueBox(
-                x: this.xPositionOnScreen,
-                y: this.yPositionOnScreen - Game1.tileSize + Game1.tileSize / 8,
-                width: this.width,
-                height: this.height + Game1.tileSize,
+                x: dialogueBox.X,
+                y: dialogueBox.Y,
+                width: dialogueBox.Width,
+                height: dialogueBox.Height,
                 speaker: false,
                 drawOnlyBox: true,
                 message: null,
                 objectDialogueWithPortrait: false);
-            base.drawBorderLabel(b, "News Bulletin", Game1.smallFont, this.xPositionOnScreen + Game1.tileSize/2, this.yPositionOnScreen - Game1.tileSize + Game1.tileSize / 8);
+            base.drawBorderLabel(b, "News Bulletin", Game1.smallFont, labelPt.X, labelPt.Y);
 
             // Draw contents
             if (true)
             {
-                for (int i = 0; i < content.Count; i++)
+                for (int i = 0; i < this.events.Count; i++)
                 {
-                    EconEvent e = content[i].Item1;
-                    Rectangle rect = content[i].Item2;
-                    if (i != content.Count - 1)
+                    EconEvent e = this.events[i];
+                    if (i != this.events.Count - 1)
                     {
-                        this.drawHorizontalPartition(b, rect.Bottom - (Game1.tileSize - this.separatorHeight) / 2, true);
+                        this.drawHorizontalPartition(b, this.dims.SeparatorYPosition(i), true);
                     }
 
-                    Utility.DrawWrappedString(b, Game1.dialogueFont, e.Headline, rect, Color.Black);
-                } 
+                    var sectionBox = this.dims.SectionBox(i);
+                    Utility.DrawWrappedString(b, Game1.dialogueFont, e.Headline, sectionBox, Color.Black);
+                }
             }
             else
             {
@@ -89,11 +146,33 @@ namespace StardewEcon.View
                 //this.drawIconGrid(b, EconEventFactory._Crops);
             }
 
+            // Test code to test dimensions:
+            //var flatColor = Game1.fadeToBlackRect;
+            //b.Draw(flatColor, this.dims.OuterBox(),    Color.Blue * 0.5f);
+            //b.Draw(flatColor, this.dims.DialogueBox(), Color.Red * 0.5f);
+            //b.Draw(flatColor, this.dims.InnerBox(),    Color.Green * 0.5f);
+            //
+            //for (int i = 0; i < this.events.Count; i++)
+            //{
+            //    var box = this.dims.SectionBox(i);
+            //    b.Draw(flatColor, box, Color.Green * 0.5f);
+
+            //    var lineY = this.dims.SeparatorYPosition(i);
+            //    var line = new Rectangle
+            //    {
+            //        X = box.X,
+            //        Y = lineY,
+            //        Width = box.Width,
+            //        Height = 1 * Game1.pixelZoom,
+            //    };
+            //    b.Draw(flatColor, line, Color.Red * 0.5f);
+            //}
+
             // Draw close box and mouse
             base.draw(b);
             this.drawMouse(b);
 
-            // Draw hover text!
+            // Draw hover text
             this.hoverBox?.Draw(b);
         }
 
@@ -106,12 +185,10 @@ namespace StardewEcon.View
         {
             base.performHoverAction(x, y);
             this.hoverBox = null;
-            int i = 0;
-            foreach (var content in GetContent())
+            foreach (var content in this.events.Zip(this.dims.SectionBoxes(), Tuple.Create))
             {
                 EconEvent e = content.Item1;
                 Rectangle rect = content.Item2;
-                i++;
                 if ( rect.Contains(x, y) )
                 {
                     if (e.AffectedItem > -1)
@@ -123,22 +200,20 @@ namespace StardewEcon.View
             }
         }
 
-        private IEnumerable<Tuple<EconEvent, Rectangle>> GetContent()
-        {
-            int x = this.xPositionOnScreen + this.xOffsetToInternal;
-            int y = this.yPositionOnScreen + this.yOffsetToInternal;
-            int dy = this.itemHeight + this.separatorHeight;
-            return this.events.Select((e, i) => Tuple.Create(e, new Rectangle(x, y + i * dy, this.internalWidth, this.itemHeight)));
-        }
-
         private void DrawIconGrid(SpriteBatch b, IReadOnlyList<int> items)
         {
-            int startX = this.xPositionOnScreen + this.xOffsetToInternal;
-            int startY = this.yPositionOnScreen + this.yOffsetToInternal;
             int iconWidth = 16 * Game1.pixelZoom;
             int iconHeight = 16 * Game1.pixelZoom;
-            int dx = iconWidth;
-            int dy = iconHeight;
+            int colSep = 0 * Game1.pixelZoom;
+            int rowSep = 0 * Game1.pixelZoom;
+
+            var innerBox = this.dims.InnerBox();
+            int startX = innerBox.X;
+            int startY = innerBox.Y;
+            int dx = iconWidth + colSep;
+            int dy = iconHeight + rowSep;
+
+            int maxIconsPerRow = (innerBox.Width + colSep) / dx;
 
             int x = 0;
             int y = 0;
@@ -157,11 +232,209 @@ namespace StardewEcon.View
                     color: Color.White);
 
                 x++;
-                if (x >= 6)
+                if (x >= maxIconsPerRow)
                 {
                     x = 0;
                     y++;
                 }
+            }
+        }
+
+        /**
+         * <summary>Private class to help us manage the dimensions of the menu.</summary>
+         */
+        private class MenuDimensions
+        {
+            // Height of horizontal separator, in game pixels
+            private const int separatorHeight = 4;
+
+            // Width of section, in game pixels
+            private int sectionWidth = 100;
+            
+            // Height of section, in game pixels
+            private int sectionHeight = 50;
+
+            // Number of items (sections)
+            private int numItems;
+
+            // Number of screen pixels wide a game pixel is
+            private int pixelZoom;
+
+            public MenuDimensions(int numItems, int pixelZoom)
+            {
+                this.numItems = numItems;
+                this.SetZoom(pixelZoom);
+            }
+
+            public void SetZoom(int pixelZoom)
+            {
+                this.pixelZoom = pixelZoom;
+            }
+
+            /**
+             * <summary>The dimensions of the menu, in screen pixels.</summary>
+             * <remarks>
+             *  This is used to set our internal parameters, which do things like
+             *  position the close button.
+             * </remarks>
+             */
+            public Rectangle OuterBox()
+            {
+                var innerHeight = this.InnerHeight();
+                var extraVertical = this.ExtraVerticalSpace();
+
+                var width  = this.sectionWidth * this.pixelZoom + IClickableMenu.borderWidth * 2;
+                var height = innerHeight * this.pixelZoom + IClickableMenu.borderWidth * 2 + extraVertical;
+                return new Rectangle
+                {
+                    X = (Game1.viewport.Width - width) / 2,
+                    Y = (Game1.viewport.Height - height) / 2,
+                    Width = width,
+                    Height = height
+                };
+            }
+
+            /**
+             * <summary>Dimensions of the dialogue box, in screen pixels.</summary>
+             * <remarks>
+             *  We use this when actually drawing the dialogue box.
+             * </remarks>
+             */
+            public Rectangle DialogueBox()
+            {
+                var outerBox = this.OuterBox();
+                var upperMargin = this.DialogueUpperMargin();
+                return new Rectangle
+                {
+                    X = outerBox.X,
+                    // For whatever reason, the dialogue box has a HUGE upper margin.
+                    Y = outerBox.Y - upperMargin,
+                    Width = outerBox.Width,
+                    Height = outerBox.Height + upperMargin,
+                };
+            }
+
+            /**
+             * <summary>Dimensions of the space inside the menu, in screen pixels.</summary>
+             * <remarks>
+             *  Content to appear in the menu must be within this rectangle.
+             * </remarks>
+             */
+            public Rectangle InnerBox()
+            {
+                var outerBox = this.OuterBox();
+                var extraVertical = this.ExtraVerticalSpace();
+                return new Rectangle()
+                {
+                    X = outerBox.X + IClickableMenu.borderWidth,
+                    Y = outerBox.Y + IClickableMenu.borderWidth + extraVertical,
+                    Width = this.sectionWidth * this.pixelZoom,
+                    Height = this.InnerHeight() * this.pixelZoom,
+                };
+            }
+
+            /**
+             * <summary>Dimensions of a section's box, in screen pixels</summary>
+             * 
+             * <param name="index">Which box to return.</param>
+             */
+            public Rectangle SectionBox(int index)
+            {
+                var innerBox = this.InnerBox();
+                var secHeight = this.SectionHeight();
+                int dy = secHeight + this.SeparatorHeight();
+
+                return new Rectangle
+                {
+                    X = innerBox.X,
+                    Y = innerBox.Y + index * dy,
+                    Width = innerBox.Width,
+                    Height = secHeight,
+                };
+            }
+
+            /**
+             * <summary>Enumerates over all section boxes</summary>
+             */
+            public IEnumerable<Rectangle> SectionBoxes()
+            {
+                // Used nowhere but GetContent(), which is used nowhere but the hover check.
+                for (int i = 0; i < this.numItems; i++)
+                {
+                    yield return this.SectionBox(i);
+                }
+            }
+
+            /**
+             * <summary>Y position to draw a specific section separator at, in screen pixels.</summary>
+             * 
+             * <param name="index">Which separator; appears below SectionBox(index).</param>
+             */
+            public int SeparatorYPosition(int index)
+            {
+                return SectionBox(index).Bottom - this.DividerUpperMargin();
+            }
+
+            /**
+             * <summary>Where to place the menu label, in screen pixels.</summary>
+             */
+            public Point MenuLabelPoint()
+            {
+                var dialogueBox = this.DialogueBox();
+                return new Point
+                {
+                    X = dialogueBox.X + (8 * this.pixelZoom),
+                    Y = dialogueBox.Y,
+                };
+            }
+
+            /**
+             * <summary>How tall a section is, in screen pixels.</summary>
+             */
+            private int SectionHeight()
+            {
+                return this.sectionHeight * this.pixelZoom;
+            }
+
+            /**
+             * <summary>How tall a separator is, in screen pixels.</summary>
+             */
+            private int SeparatorHeight()
+            {
+                return MenuDimensions.separatorHeight * this.pixelZoom;
+            }
+
+            /**
+             * <summary>How tall the inner menu space is, in *game* pixels.</summary>
+             */
+            private int InnerHeight()
+            {
+                return (numItems) * this.sectionHeight + (numItems - 1) * MenuDimensions.separatorHeight;
+            }
+
+            /**
+             * <summary>The margin above the dialogue we need to add for it to appear where we expect, in screen pixels.</summary>
+             */
+            private int DialogueUpperMargin()
+            {
+                return 16 * this.pixelZoom;
+            }
+
+            /**
+             * <summary>The margin above the divider we need to account for for it to appear where we expect, in screen pixels.</summary>
+             */
+            private int DividerUpperMargin()
+            {
+                // TODO: Make this a function of divider height?
+                return 6 * this.pixelZoom;
+            }
+
+            /**
+             * <summary>The extra vertical space we have to add to clear the bottom of the menu label, in screen pixels.</summary>
+             */
+            private int ExtraVerticalSpace()
+            {
+                return 4 * this.pixelZoom;
             }
         }
     }
--- StardewEcon/src/View/NewsBulletinObject.cs
+++ StardewEcon/src/View/NewsBulletinObject.cs
@@ -6,12 +6,41 @@ using StardewEcon.Econ;
 
 namespace StardewEcon.View
 {
+    /**
+     * <summary>An interactive object that displays the News Bulletin.</summary>
+     * <remarks>
+     *  This is an invisible object placed on top of the tile outside of Pierre's
+     *  shop that normally shows you his hours. When interacted with, it will
+     *  bring up the News Bulletin menu.
+     *  
+     *  This object is not to be saved with the game. It may cause the game to
+     *  panic if it is in the objects list when the game is saved or loaded.
+     *  Please call <see cref="RemoveBeforeSaving"/> before the game is saved
+     *  and <see cref="ReplaceAfterSaving"/> after the game is saved.
+     * </remarks>
+     */
     class NewsBulletinObject : StardewValley.Object
     {
+        /**
+         * <summary>The event manager managing the events to display.</summary>
+         */
         public readonly EconEventManager eventManager;
 
+        /**
+         * <summary>A reference to the area this object is hosted in.</summary>
+         * <remarks>
+         *  This is here simply so that we don't need to provide it with each
+         *  call to <see cref="RemoveBeforeSaving"/> and <see cref="ReplaceAfterSaving"/>,
+         *  which is pretty lazy. Luckily, this doesn't change during gameplay.
+         * </remarks>
+         */
         private GameLocation area;
 
+        /**
+         * <summary>Create a new NewsBulletinObject that draws its events from the given manager.</summary>
+         * 
+         * <param name="eventManager">The event manager that provides the current events.</param>
+         */
         public NewsBulletinObject(EconEventManager eventManager)
         {
             this.eventManager = eventManager;
@@ -29,6 +58,20 @@ namespace StardewEcon.View
             this.questItem = false;
         }
 
+        /**
+         * <summary>Places ourself into the given area.</summary>
+         * <remarks>
+         *  This tells both ourself and the given area where we are. We save the
+         *  area parameter in order to use it later when removing and replacing
+         *  ourself in the area around save operations.
+         * </remarks>
+         * 
+         * <param name="area">The game area to place ourself in.</param>
+         * <param name="loc">The tile location within the game area to place ourself at.</param>
+         * 
+         * <seealso cref="RemoveBeforeSaving"/>
+         * <seealso cref="ReplaceAfterSaving"/>
+         */
         public void SetInTown(GameLocation area, Vector2 loc)
         {
             this.area = area;
@@ -38,6 +81,16 @@ namespace StardewEcon.View
             this.boundingBox.Y = (int) loc.Y * Game1.tileSize;
         }
 
+        /**
+         * <summary>Temporarily removes ourself from our game area.</summary>
+         * <remarks>
+         *  This should come before a save operation. This exists to prevent any
+         *  kind of (de)serialization panic from the game not understanding how
+         *  to deal with this class.
+         * </remarks>
+         * 
+         * <seealso cref="ReplaceAfterSaving"/>
+         */
         public void RemoveBeforeSaving()
         {
             if( this.area?.Objects?[this.TileLocation] == this )
@@ -46,11 +99,31 @@ namespace StardewEcon.View
             }
         }
 
+        /**
+         * <summary>Replaces ourself in our last known game area and location.</summary>
+         * <remarks>
+         *  This should come after a save operation.
+         * </remarks>
+         * 
+         * <seealso cref="RemoveBeforeSaving"/>
+         */
         public void ReplaceAfterSaving()
         {
             this.area?.Objects?.Add(this.TileLocation, this);
         }
 
+        /**
+         * <summary>Checks whether this object could perform an action, and optionally performs it.</summary>
+         * <remarks>
+         *  You'll have to read the game source code to understand how this function works.
+         *  It's somehow different from <see cref="isActionable(StardewValley.Farmer)"/>.
+         * </remarks>
+         * 
+         * <param name="who">The Farmer interacting with the object.</param>
+         * <param name="justCheckingForActivity">Whether or not to perform any actions.</param>
+         * 
+         * <seealso cref="isActionable(StardewValley.Farmer)"/>
+         */
         public override bool checkForAction(StardewValley.Farmer who, bool justCheckingForActivity = false)
         {
             if (justCheckingForActivity)
@@ -62,9 +135,18 @@ namespace StardewEcon.View
             return true;
         }
 
+        /**
+         * <summary>Checks whether this object could perform an action.</summary>
+         * <remarks>
+         *  You'll have to read the game source code to understand how this function works.
+         *  It's somehow different from <see cref="checkForAction(StardewValley.Farmer, bool)"/>.
+         *  
+         *  That said, it seems like this code is only used when mousing over an object.
+         * </remarks>
+         */
         public override bool isActionable(StardewValley.Farmer who)
         {
-            // Force inspectable icon
+            // HACK: Force inspectable icon on this and the tile above it.
             if (Game1.currentCursorTile.Equals(this.TileLocation)
                 || Game1.currentCursorTile.Equals(this.TileLocation - new Vector2(0, 1))) {
                 Game1.isInspectionAtCurrentCursorTile = true;
@@ -72,67 +154,117 @@ namespace StardewEcon.View
             return true;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         * <remarks>
+         *  Day updates are handled in <see cref="ModEntry"/>.
+         * </remarks>
+         */
         public override void DayUpdate(GameLocation location)
         {
-            // TODO: Maybe handle telling the manager what to do here? Dunno.
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void draw(SpriteBatch spriteBatch, int x, int y, float alpha = 1)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void draw(SpriteBatch spriteBatch, int xNonTile, int yNonTile, float layerDepth, float alpha = 1)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void drawAttachments(SpriteBatch b, int x, int y)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void drawPlacementBounds(SpriteBatch spriteBatch, GameLocation location)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void drawWhenHeld(SpriteBatch spriteBatch, Vector2 objectPosition, StardewValley.Farmer f)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to force impassibility.</summary>
+         */
         public override bool isPassable()
         {
             return false;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         * <remarks>
+         *  We don't want tools to do anything to this object.
+         * </remarks>
+         */
         public override bool performToolAction(Tool t)
         {
             return false;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         * <remarks>
+         *  There is no reasonable action to perform here.
+         * </remarks>
+         */
         public override bool performObjectDropInAction(Object dropIn, bool probe, StardewValley.Farmer who)
         {
             return false;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void updateWhenCurrentLocation(GameTime time)
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         */
         public override void actionOnPlayerEntry()
         {
             return;
         }
 
+        /**
+         * <summary>Overrides to do nothing.</summary>
+         * <remarks>
+         *  We do not want anything to remove this object except ourself.
+         * </remarks>
+         */
         public override void performRemoveAction(Vector2 tileLocation, GameLocation environment)
         {
             return;
